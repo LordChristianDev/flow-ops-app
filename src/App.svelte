36b98306f0143e8635f20eac1a5fcd3ab75a7d7c
LordChristianDev <lordchristian.dev@gmail.com>
2025-11-17 04:00:30 +0800

    Updated Application Layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-  import Router from 'svelte-spa-router';
-  import AuthenticatedLayout from '@/components/layout/Authenticated.svelte';
-  import routes from '@/router';
-  import { location } from 'svelte-spa-router';
-  
-  // Check if current route needs auth
-  $: needsAuth = $location !== '/login' && $location !== '*';
+	import './app.css';
+	import Router from 'svelte-spa-router';
+	import AuthenticatedLayout from '@/components/layout/Authenticated.svelte';
+	import routes from '@/router';
+	import { location } from 'svelte-spa-router';
+
+	// Check if current route needs auth
+	$: needsAuth = $location !== '/login' && $location !== '*';
 </script>
 
 {#if needsAuth}
-  <AuthenticatedLayout>
-    <Router {routes} />
-  </AuthenticatedLayout>
+	<AuthenticatedLayout>
+		<Router {routes} />
+	</AuthenticatedLayout>
 {:else}
-  <Router {routes} />
+	<Router {routes} />
 {/if}
