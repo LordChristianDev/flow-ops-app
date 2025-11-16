e68ad8723f1460e72fc7d6b8f22d9ef9f8592e6f
LordChristianDev <lordchristian.dev@gmail.com>
2025-11-17 03:59:51 +0800

    Integrated Svelte ShadCN

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, loadEnv } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  return {
-    plugins: [svelte()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      }
-    },
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true
-    },
-  };
+	return {
+		plugins: [tailwindcss(), svelte()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+				$lib: path.resolve("./src/lib"),
+			},
+		},
+		build: { outDir: 'dist', emptyOutDir: true },
+	};
 });
