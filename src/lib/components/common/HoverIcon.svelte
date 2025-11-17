d3f0dc4bed0c5a43be410f909f37075c478e7ec0
LordChristianDev <lordchristian.dev@gmail.com>
2025-11-17 14:16:06 +0800

    Added Hover Icon to Common Components

---
+++ src/lib/components/common/HoverIcon.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Columns2 } from "@lucide/svelte";
+  import { cn } from "$lib/utils";
+  import { Button } from "$lib/components/ui/button";
+
+  export let src: string;
+  export let className: string = "";
+  export let toggle: () => void;
+
+  let isHovered = false;
+</script>
+
+<Button
+  variant="ghost"
+  size="icon"
+  onclick={toggle}
+  aria-label="Toggle sidebar"
+  class="text-muted-foreground cursor-pointer"
+  onmouseenter={() => isHovered = true}
+  onmouseleave={() => isHovered = false}
+>
+  {#if isHovered}
+    <Columns2 />
+  {:else}
+    <img 
+		src={src} 
+		alt="toggle-image" 
+		aria-hidden="true"
+		class={cn("", className)} 
+	/>
+  {/if}
+</Button>
