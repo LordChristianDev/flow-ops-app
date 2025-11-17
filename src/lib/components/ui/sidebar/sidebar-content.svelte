dbefedef393d49e76bca76842e1b0b80cad9ffae
LordChristianDev <lordchristian.dev@gmail.com>
2025-11-17 14:18:26 +0800

    Updated Sidebar Design

--- src/lib/components/ui/sidebar/sidebar-content.svelte
+++ src/lib/components/ui/sidebar/sidebar-content.svelte
@@ -15,7 +15,7 @@
 	data-slot="sidebar-content"
 	data-sidebar="content"
 	class={cn(
-		"flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+		"pt-16 flex max-h-[calc(100vh-56px)] flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
 		className
 	)}
 	{...restProps}
